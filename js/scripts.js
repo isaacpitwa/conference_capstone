2897dee818124a9c1b0eafe1297e2650171a8cb6
isaacpitwa <isaacpitwa256@gmail.com>
2022-03-08 22:21:25 +0300

    dynamic featured speakers

--- js/scripts.js
+++ js/scripts.js
@@ -22,32 +22,32 @@ const speakers = [
         description: `As the author of &lt;Digital Art Art of Our Time&gt;, he opened 'Art Center Nabi', Korea's first digital art institution in 2000, and is currently serving.`
     },
     {
-        name:'Jeon Gil-nam',
-        tag_name:'Kilnam Chon',
-        picture:'images/speakers/speaker_04.png',
-        workplace: 'Emeritus Professor, Korea Advanced Institute of Science and Technology (KAIST)',
-        description: `By developing Asia's first Internet protocol network SDN and leading Korea's first private line Internet connection in 1990, it ushered in the era of the Internet in earnest.`
-    },
-    {
         name:'Julia Leda',
         tag_name:'Julia Leda',
-        picture:'images/speakers/speaker_05.png',
-        workplace: 'Professor at Harvard Law School',
-        description: `Focusing on a collaborative approach in a networked environment, he created the concept of
-        co-production based on sharing, such as open source software and Wikipedia.`
+        picture:'images/speakers/speaker_04.png',
+        workplace: 'Head of the Young Pirates of Europe',
+        description: `European integration and online youth participation in politics and democracy are major concerns, and a report has been published that will potentially affect the revision of the EU's copyright law in July.`
     },
     {
-        name:'Jeon Gil-nam',
-        tag_name:'Kilnam Chon',
+        name:'Layla Tretikov',
+        tag_name:'Layla Tretikov',
+        picture:'images/speakers/speaker_05.png',
+        workplace: 'Secretary General of the Wikimedia Foundation',
+        description: `Layla Tretikov is the general secretary of the Wikimedia Foundation, a non-profit organization that runs Wikipedia.`
+    },
+   
+    {
+        name:'Ryan Merkley',
+        tag_name:'Ryan Merkley',
         picture:'images/speakers/speaker_06.png',
-        workplace: 'Emeritus Professor, Korea Advanced Institute of Science and Technology (KAIST)',
-        description: `By developing Asia's first Internet protocol network SDN and leading Korea's first private line Internet connection in 1990, it ushered in the era of the Internet in earnest.`
+        workplace: 'Creative Commons CEO, Former Mozilla Foundation COO',
+        description: `He has been active in open movements such as open government and open source.`
     }
 ]
 const speakersList = document.getElementById('speakers-list');
 
 window.onload = (params) => {
-speakers.forEach((speaker)=>{
+speakers.reverse().forEach((speaker)=>{
     const speakerHTML = `<div class="speaker clearfix">
                             <img src=${speaker.picture} alt="${speaker.name}"/>
                             <div class="desc">
