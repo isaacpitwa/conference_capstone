7312a78122cbac87538dc5d03646083c1b1a2459
isaacpitwa <isaacpitwa256@gmail.com>
2022-03-10 01:27:37 +0300

    add show more speakers button

--- js/scripts.js
+++ js/scripts.js
@@ -46,9 +46,14 @@ const speakers = [
   },
 ];
 const speakersList = document.getElementById('speakers-list');
+let speakersToShow = speakers;
 
 window.onload = (params) => {
-  speakers.reverse().forEach((speaker) => {
+  speakersList.innerHTML = '';
+  if (window.screen.width <= 768) {
+    speakersToShow = speakers.slice(0, 2);
+  }
+  speakersToShow.reverse().forEach((speaker) => {
     const speakerHTML = `<div class="speaker clearfix">
                             <img src=${speaker.picture} alt="${speaker.name}"/>
                             <div class="desc">
@@ -62,6 +67,46 @@ window.onload = (params) => {
     speakersList.insertAdjacentHTML('afterbegin', speakerHTML);
   });
 };
+window.addEventListener('resize', (event) => {
+  speakersList.innerHTML = '';
+  if (window.screen.width <= 768) {
+    speakersToShow = speakers.slice(0, 2);
+  } else {
+    speakersToShow = speakers;
+  }
+  speakersToShow.reverse().forEach((speaker) => {
+    const speakerHTML = `<div class="speaker clearfix">
+                                <img src=${speaker.picture} alt="${speaker.name}"/>
+                                <div class="desc">
+                                    <p href="#" class="speaker-name">${speaker.name}</p>
+                                    <p href="#" class="speaker-tag-name">${speaker.tag_name}</p>
+                                    <p href="#" class="speaker-job">${speaker.workplace}</p>
+                                    <p class="speaker-desc">${speaker.description}</p>
+    
+                                </div>
+                            </div>`;
+    speakersList.insertAdjacentHTML('afterbegin', speakerHTML);
+  });
+});
+
+// shows more speakers
+function showMoreSpeakers() {
+  speakersList.innerHTML = '';
+  speakers.reverse().forEach((speaker) => {
+    const speakerHTML = `<div class="speaker clearfix">
+                                    <img src=${speaker.picture} alt="${speaker.name}"/>
+                                    <div class="desc">
+                                        <p href="#" class="speaker-name">${speaker.name}</p>
+                                        <p href="#" class="speaker-tag-name">${speaker.tag_name}</p>
+                                        <p href="#" class="speaker-job">${speaker.workplace}</p>
+                                        <p class="speaker-desc">${speaker.description}</p>
+        
+                                    </div>
+                                </div>`;
+    speakersList.insertAdjacentHTML('afterbegin', speakerHTML);
+    document.getElementById('more-btn').classList.add('hidden');
+  });
+}
 // Toggles Mobile Menu
 function toggleMenu() {
   document.getElementById('toggle-menu').classList.toggle('toggle-menu');
