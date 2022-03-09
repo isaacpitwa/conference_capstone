6c9ee7c3b3bb87df71343612c15dc817f1eff5a8
isaacpitwa <isaacpitwa256@gmail.com>
2022-03-09 15:54:26 +0300

    Fix js lint errors

--- js/scripts.js
+++ js/scripts.js
@@ -1,53 +1,54 @@
+/* eslint-disable no-unused-vars */
 const speakers = [
-    {
-        name:'Yochai Benkler',
-        tag_name:'yohai benkler',
-        picture:'images/speakers/speaker_01.png',
-        workplace: 'Professor at Harvard Law School',
-        description: `Focusing on a collaborative approach in a networked environment, he created the concept of
-        co-production based on sharing, such as open source software and Wikipedia.`
-    },
-    {
-        name:'Jeon Gil-nam',
-        tag_name:'Kilnam Chon',
-        picture:'images/speakers/speaker_02.png',
-        workplace: 'Emeritus Professor, Korea Advanced Institute of Science and Technology (KAIST)',
-        description: `By developing Asia's first Internet protocol network SDN and leading Korea's first private line Internet connection in 1990, it ushered in the era of the Internet in earnest.`
-    },
-    {
-        name:'Noh So-young',
-        tag_name:'Sohyeong Noh',
-        picture:'images/speakers/speaker_03.png',
-        workplace: 'Art Center Nabi Director, CC Korea Director',
-        description: `As the author of &lt;Digital Art Art of Our Time&gt;, he opened 'Art Center Nabi', Korea's first digital art institution in 2000, and is currently serving.`
-    },
-    {
-        name:'Julia Leda',
-        tag_name:'Julia Leda',
-        picture:'images/speakers/speaker_04.png',
-        workplace: 'Head of the Young Pirates of Europe',
-        description: `European integration and online youth participation in politics and democracy are major concerns, and a report has been published that will potentially affect the revision of the EU's copyright law in July.`
-    },
-    {
-        name:'Layla Tretikov',
-        tag_name:'Layla Tretikov',
-        picture:'images/speakers/speaker_05.png',
-        workplace: 'Secretary General of the Wikimedia Foundation',
-        description: `Layla Tretikov is the general secretary of the Wikimedia Foundation, a non-profit organization that runs Wikipedia.`
-    },
-   
-    {
-        name:'Ryan Merkley',
-        tag_name:'Ryan Merkley',
-        picture:'images/speakers/speaker_06.png',
-        workplace: 'Creative Commons CEO, Former Mozilla Foundation COO',
-        description: `He has been active in open movements such as open government and open source.`
-    }
-]
+  {
+    name: 'Yochai Benkler',
+    tag_name: 'yohai benkler',
+    picture: 'images/speakers/speaker_01.png',
+    workplace: 'Professor at Harvard Law School',
+    description: `Focusing on a collaborative approach in a networked environment, he created the concept of
+        co-production based on sharing, such as open source software and Wikipedia.`,
+  },
+  {
+    name: 'Jeon Gil-nam',
+    tag_name: 'Kilnam Chon',
+    picture: 'images/speakers/speaker_02.png',
+    workplace: 'Emeritus Professor, Korea Advanced Institute of Science and Technology (KAIST)',
+    description: 'By developing Asia\'s first Internet protocol network SDN and leading Korea\'s first private line Internet connection in 1990, it ushered in the era of the Internet in earnest.',
+  },
+  {
+    name: 'Noh So-young',
+    tag_name: 'Sohyeong Noh',
+    picture: 'images/speakers/speaker_03.png',
+    workplace: 'Art Center Nabi Director, CC Korea Director',
+    description: 'As the author of &lt;Digital Art Art of Our Time&gt;, he opened \'Art Center Nabi\', Korea\'s first digital art institution in 2000, and is currently serving.',
+  },
+  {
+    name: 'Julia Leda',
+    tag_name: 'Julia Leda',
+    picture: 'images/speakers/speaker_04.png',
+    workplace: 'Head of the Young Pirates of Europe',
+    description: 'European integration and online youth participation in politics and democracy are major concerns, and a report has been published that will potentially affect the revision of the EU\'s copyright law in July.',
+  },
+  {
+    name: 'Layla Tretikov',
+    tag_name: 'Layla Tretikov',
+    picture: 'images/speakers/speaker_05.png',
+    workplace: 'Secretary General of the Wikimedia Foundation',
+    description: 'Layla Tretikov is the general secretary of the Wikimedia Foundation, a non-profit organization that runs Wikipedia.',
+  },
+
+  {
+    name: 'Ryan Merkley',
+    tag_name: 'Ryan Merkley',
+    picture: 'images/speakers/speaker_06.png',
+    workplace: 'Creative Commons CEO, Former Mozilla Foundation COO',
+    description: 'He has been active in open movements such as open government and open source.',
+  },
+];
 const speakersList = document.getElementById('speakers-list');
 
 window.onload = (params) => {
-speakers.reverse().forEach((speaker)=>{
+  speakers.reverse().forEach((speaker) => {
     const speakerHTML = `<div class="speaker clearfix">
                             <img src=${speaker.picture} alt="${speaker.name}"/>
                             <div class="desc">
@@ -57,12 +58,11 @@ speakers.reverse().forEach((speaker)=>{
                                 <p class="speaker-desc">${speaker.description}</p>
 
                             </div>
-                        </div>`
-    speakersList.insertAdjacentHTML('afterbegin',speakerHTML)
-    
-})
-}
+                        </div>`;
+    speakersList.insertAdjacentHTML('afterbegin', speakerHTML);
+  });
+};
 // Toggles Mobile Menu
 function toggleMenu() {
-    document.getElementById('toggle-menu').classList.toggle('toggle-menu');
+  document.getElementById('toggle-menu').classList.toggle('toggle-menu');
 }
